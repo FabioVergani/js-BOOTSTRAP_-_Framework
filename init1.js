65529d9dca719206c2ae4239ec4e380a9b84ebd4
FabioVergani <fabio.vergani.1975@gmail.com>
2015-05-25 00:19:38 +0200

    Update init1.js

--- init1.js
+++ init1.js
@@ -1,9 +1,14 @@
 (function(global){'use strict';
+ function consume(arraylike,callback){var m=arraylike,f=callback;while(m.length!==0){f(m.shift());};};
+
  var $=global;
  if($){
+
 	var $w=$,
 	functionPrototype, isFunction, callBind,
-	objectPrototype, createObject, freezeObject, emptyObject, has, hasOwnProperty;
+	objectPrototype, createObject, freezeObject, emptyObject, has, hasOwnProperty,
+	console;
+
 
 
 	(function($Function){
@@ -26,23 +31,65 @@
 
 	has=freezeObject(emptyObject({
 	 'OwnProperty':{value:function(e,i){var o=e,p=i;return p in o && hasOwnProperty(o,p);}},
-	 'OwnMethod':{value:function(e,i){var o=e,p=i;return hasOwnProperty(o,p) && isFunction(o[p]);}}
+	 'OwnMethod':{value:function(e,i){var o=e,p=i;return has.OwnProperty(o,p) && isFunction(o[p]);}}
 	}));
 
 
-
-console.dir(has);
-
-/*
-*/
-
-/*
-var =,;
-console.log();
-,…
-*/
+	(function(a,b){
+	 function test(have){return function(obj,prop,v){var o=obj,p=prop,x=have(o,p);if(x===false){o[p]=v;};return !x;};};
+	 var object2fill,prop2found,filled,methods=test,fillPropertyWith=methods(a),fillMethodWith=methods(b);
 
 
+
+	 prop2found='console';
+	 object2fill=$w;
+	 methods=[
+		'trace','table','log','clear','dir','dirxml',
+		'warn','error','exception','info','count',
+		'group','groupCollapsed','groupEnd',
+		'profile','profileEnd','markTimeline',
+		'time','timeEnd','timeStamp',
+		'debug','assert'
+	 ];
+
+	 if(fillPropertyWith(object2fill,prop2found,emptyObject())){
+		(function(object,array){
+		 var o=filled=object,m=array;
+		 consume(m,function(i){o[i]=function(){};});
+		})(object2fill[prop2found],methods);
+	 }else{
+
+		(function(object,array){
+		 var o=filled=object,m=array;
+		 consume(m,function(i){fillMethodWith(o,i,function(){});});
+		})(object2fill[prop2found],methods);
+
+	 };
+
+	 $w.console.dir(console);
+
+	})(has.OwnProperty,has.OwnMethod);
+
+	/*
+	 ...
+	*/
  }
 })(document.defaultView||window);
 
+
+
+/*console.dir();
+var =,;
+console.log();
+,…
+
+
+=====================================================
+function cally(x){return Function.prototype.call.bind(x);
+
+is.something($w)
+is={
+	defined:function(obj){var e=obj;return e!=='' && typeof(e)!=='undefined';},
+	something:function(obj){var e=obj;return e!==null && this.defined(e);}/*,…* /
+ };
+*/
