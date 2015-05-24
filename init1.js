ef45d7722f206938db44dd7458282385312518bf
FabioVergani <fabio.vergani.1975@gmail.com>
2015-05-24 20:36:01 +0200

    Create init1.js

---
+++ init1.js
@@ -0,0 +1,20 @@
+(function(global){'use strict';
+ var $=global;
+ if($){
+	var $w=$, createObject, emptyObject;
+
+	(function($Object){
+	 var O=$Object,
+	 create=createObject=O.create,
+	 empty=emptyObject=function(){return create(null);};
+	})($w.Object);
+
+
+
+/*-*/
+//console.log();
+/*,…*/
+
+
+ }
+})(document.defaultView||window);
