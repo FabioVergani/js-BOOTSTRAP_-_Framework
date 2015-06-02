3871a251e0b17e5609b8cf4f23d07e8d030876de
FabioVergani <fabio.vergani.1975@gmail.com>
2015-06-02 23:21:04 +0200

    Update valueOf.js

--- valueOf.js
+++ valueOf.js
@@ -1,1 +1,15 @@
  function isObject(e){return {theObject:e,v:typeof(e)==='object',valueOf:function(){return this.v;}};}
+ 
+ 
+ 
+the typical use of protoype-based objects:
+
+// define a new type of object
+var foo = function() {};  
+
+// define a default attribute and value that all objects of this type will have
+foo.prototype.attribute1 = "defaultValue1";  
+
+// create a new object of my type
+var emptyObj = new foo();
+console.log(emptyObj.attribute1);       // outputs defaultValue1
