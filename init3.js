d5c163825f206e8963be19f49873dcc2509dbc67
FabioVergani <fabio.vergani.1975@gmail.com>
2015-06-06 13:32:05 +0200

    Create init3.js

---
+++ init3.js
@@ -0,0 +1,15 @@
+//noprotect
+console.clear();
+//#START
+//javascript:
+void((function(gs){'use strict';
+ function isObject(e){return typeof(e)==='object';}
+ var e,w=gs,O=w.Object,p='NameSpaceUniqueId';
+ ((p in w && w.hasOwnProperty(p) && isObject(e=w[p]) && O.isFrozen(e))?e:(e={writable:true,value:0},
+ w[p]=O.seal(O.create(null,{calls:e,run:e,use:{value:function(gs){
+
+	/**/
+
+ }}})))).use(w);
+})(window));
+//#END
