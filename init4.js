52da54480b00c29ec154cfbf4c10dc0e068501e2
FabioVergani <fabio.vergani.1975@gmail.com>
2015-06-06 14:16:55 +0200

    Create init4.js

---
+++ init4.js
@@ -0,0 +1,19 @@
+//noprotect
+console.clear();
+//#START
+//javascript:
+void((function(gs){'use strict';
+ var e,w=gs,O=w.Object,p='NameSpaceUniqueId';
+ ((p in w && w.hasOwnProperty(p) && O.isSealed(e=w[p]))?e:(e={writable:true,value:0},
+ w[p]=O.seal(O.create(null,{calls:e,run:e,use:{value:function(gs){
+	var e=this;console.dir(e);
+	if(e.run===0){
+		e.run=++e.calls;
+		//PROCESS, insertmorecodehere >e.run=0;
+	}else{
+		//wait
+	}/*;*/
+	console.log(e.calls);
+ }}})))).use(w);
+})(window));
+//#END
